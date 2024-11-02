04c21624a1cb7e5d73bd3f4e2267aaecebbec95b
Chaitu8553 <chaitanyayelijerla@gmail.com>
2024-11-02 17:47:16 +0530

    Update text1.py

--- text1.py
+++ text1.py
@@ -4,3 +4,4 @@ print("good afternoon durga")
 print("good afternoon rockey")
 print("do not angry with that")
 print("read me as soon as possible for the exams)"
+print("most important thing is rubbiesh")
